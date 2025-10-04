7eb24884d67e53b25321ff712ccbf966d92b55e3
maru <anthonyclyde.melendez@wvsu.edu.ph>
2025-10-04 15:40:04 +0800

    dddddddddddd

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,6 @@
 const nextConfig = {
   output: "export",
   trailingSlash: true,
-  basePath: process.env.NODE_ENV === "production" ? "/Spawn-Parser" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/Spawn-Parser" : "",
   images: {
     unoptimized: true,
     remotePatterns: [
